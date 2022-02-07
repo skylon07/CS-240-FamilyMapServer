33366d9263a715c7c2c56777e63108361873a169
Taylor Brown <taylor@davtar.org>
2022-02-07 09:42:25 -0700

    Add docstrings for GenericRequest/Response

--- src/main/java/services/requests/GenericRequest.java
+++ src/main/java/services/requests/GenericRequest.java
@@ -1,5 +1,10 @@
 package services.requests;
 
+/**
+ * The generic class for all requests. Currently the only purpose this serves
+ * is as a generic request datatype and a placeholder for possible changes that
+ * need to be applied to all request types.
+ */
 abstract class GenericRequest {
-    // TODO
+    // this block intentionally left blank
 }
--- src/main/java/services/responses/GenericResponse.java
+++ src/main/java/services/responses/GenericResponse.java
@@ -1,5 +1,10 @@
 package services.responses;
 
+/**
+ * The generic class for all responses. Currently the only purpose this serves
+ * is as a generic response datatype and a placeholder for possible changes that
+ * need to be applied to all response types.
+ */
 abstract class GenericResponse {
-    // TODO
+    // this block intentionally left blank
 }
