d7755a8d3128ac2d4e70ce4fbd19375d47a5ffba
Taylor Brown <taylor@davtar.org>
2022-02-07 13:06:04 -0700

    Add RO/WO constraints to GenericRequest/Response

--- src/main/java/services/requests/GenericRequest.java
+++ src/main/java/services/requests/GenericRequest.java
@@ -4,6 +4,8 @@ package services.requests;
  * The generic class for all requests. Currently the only purpose this serves
  * is as a generic request datatype and a placeholder for possible changes that
  * need to be applied to all request types.
+ * 
+ * Note that all requests should be read-only (only have getters for properties)
  */
 abstract class GenericRequest {
     // this block intentionally left blank
--- src/main/java/services/responses/GenericResponse.java
+++ src/main/java/services/responses/GenericResponse.java
@@ -4,6 +4,8 @@ package services.responses;
  * The generic class for all responses. Currently the only purpose this serves
  * is as a generic response datatype and a placeholder for possible changes that
  * need to be applied to all response types.
+ * 
+ * Note that all responses should be write-only (only have setters for properties)
  */
 abstract class GenericResponse {
     // this block intentionally left blank
