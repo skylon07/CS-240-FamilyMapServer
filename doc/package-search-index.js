36edf617dd7c72373f00d7c74ebad88fa2eecb53
Taylor Brown <taylor@davtar.org>
2022-02-07 13:56:18 -0700

    Generate documentation via javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dataAccess"},{"l":"handlers"},{"l":"models"},{"l":"services"},{"l":"services.requests"},{"l":"services.responses"}]
