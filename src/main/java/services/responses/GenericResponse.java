f7d307af970f4f7e6c7ea48640d42bec15c80277
Taylor Brown <taylor@davtar.org>
2022-02-23 12:34:03 -0700

    Conform GenericRepsonse properties to be public

--- src/main/java/services/responses/GenericResponse.java
+++ src/main/java/services/responses/GenericResponse.java
@@ -6,23 +6,7 @@ package services.responses;
  */
 abstract class GenericResponse {
     /** The response's associated message. For errors, the error message. */
-    private String message;
+    public String message;
     /** The state of a request's success or failure */
-    private boolean success;
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public boolean getSuccess() {
-        return this.success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
+    public boolean success;
 }
