67c72bd82ca792c8e77c88074f5fdff00aed37ec
Taylor Brown <taylor@davtar.org>
2022-02-23 12:47:53 -0700

    Finish the LoginService

--- src/main/java/services/LoginService.java
+++ src/main/java/services/LoginService.java
@@ -1,9 +1,15 @@
 package services;
 
 import dataAccess.Database;
+import dataAccess.DatabaseException;
+import dataAccess.UserAccessor;
+
+import models.User;
 
 import services.requests.LoginRequest;
 import services.responses.LoginResponse;
+
+import utils.AuthUtils;
 
 /**
  * This service provides functionality for the login endpoint.
@@ -20,7 +26,49 @@ public class LoginService extends GenericService<LoginRequest, LoginResponse> {
 
     @Override
     public LoginResponse onPost(LoginRequest request, Database database) throws InvalidHTTPMethodException {
-        // TODO Auto-generated method stub
-        return null;
+        String username = request.username;
+        String password = request.password;
+        assert username != null : "LoginService expected a username";
+        assert password != null : "LoginService expected a password";
+        
+        AuthUtils authUtils = new AuthUtils(database);
+        UserAccessor userAcc = new UserAccessor(database);
+        try {
+            String authToken = authUtils.authenticateUser(username, password);
+            User user = userAcc.getByUsername(username);
+            String personID = user.getPersonID();
+            return createSuccessfulResponse(authToken, username, personID);
+        } catch (DatabaseException err) {
+            return createFailedResponse(err.getMessage());
+        }
+    }
+
+    /**
+     * Creates a successful LoginResponse with required parameters
+     * 
+     * @param authToken is the newly created auth token
+     * @param username is the username of the now-logged-in User
+     * @param personID is the personID of that User
+     * @return the successful LoginResponse
+     */
+    private LoginResponse createSuccessfulResponse(String authToken, String username, String personID) {
+        LoginResponse response = new LoginResponse();
+        response.success = true;
+        response.authtoken = authToken;
+        response.username = username;
+        response.personID = personID;
+        return response;
+    }
+
+    /**
+     * Creates a failed LoginResponse with an error message
+     * @param errMsg is the message to send back in the response
+     * @return the failed LoginResponse
+     */
+    private LoginResponse createFailedResponse(String errMsg) {
+        LoginResponse response = new LoginResponse();
+        response.success = false;
+        response.message = errMsg;
+        return response;
     }
 }
