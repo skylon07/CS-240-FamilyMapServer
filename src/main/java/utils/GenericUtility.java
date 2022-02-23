19f288183222fa8c79932d650336223b061ee78e
Taylor Brown <taylor@davtar.org>
2022-02-23 12:47:34 -0700

    Create a GenericUtility class

---
+++ src/main/java/utils/GenericUtility.java
@@ -0,0 +1,17 @@
+package utils;
+
+import dataAccess.Database;
+
+abstract class GenericUtility {
+    /** The database wrapper, allowing Utilities to create Accessors */
+    protected Database database;
+
+    /**
+     * Creation process for all Utility classes
+     * 
+     * @param database is the database to use with Accessors
+     */
+    public GenericUtility(Database database) {
+        this.database = database;
+    }
+}
