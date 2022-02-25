5daa8ac7cf900df656a1bc43cda7bc7eca8d28b3
Taylor Brown <taylor@davtar.org>
2022-02-24 21:00:22 -0700

    Finish FileHandler/Service

--- src/main/java/handlers/FileHandler.java
+++ src/main/java/handlers/FileHandler.java
@@ -1,5 +1,7 @@
 package handlers;
 
+import java.net.HttpURLConnection;
+
 import com.sun.net.httpserver.*;
 
 import services.FileService;
@@ -9,11 +11,27 @@ import services.responses.FileResponse;
 public class FileHandler extends GenericHandler<FileRequest, FileResponse, FileService> {
     @Override
     protected FileRequest parseRequest(HttpExchange exchange) {
-        return null; // TODO
+        FileRequest request = new FileRequest();
+        request.path = exchange.getRequestURI().toString();
+        return request;
     }
 
     @Override
     protected FileService createBoundService() {
         return new FileService();
+    }
+
+    @Override
+    protected int getStatusCode(FileResponse response) {
+        if (response.success) {
+            return HttpURLConnection.HTTP_OK;
+        } else {
+            return HttpURLConnection.HTTP_NOT_FOUND;
+        }
+    }
+
+    @Override
+    protected String convertResponse(FileResponse response) {
+        return response.data;
     }
 }
--- src/main/java/services/FileService.java
+++ src/main/java/services/FileService.java
@@ -1,5 +1,11 @@
 package services;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import dataAccess.Database;
 import services.requests.FileRequest;
 import services.responses.FileResponse;
@@ -18,7 +24,29 @@ public class FileService extends GenericService<FileRequest, FileResponse> {
 
     @Override
     public FileResponse onGet(FileRequest request, Database database) throws InvalidHTTPMethodException {
-        // TODO Auto-generated method stub
-        return null;
+        assert request.path != null : "FileService expected a path";
+        String filePath = "web" + request.path;
+        if (filePath == "web/") {
+            filePath = "web/index.html";
+        }
+        File fileToSend = new File(filePath);
+
+        FileResponse response = new FileResponse();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileToSend));) {
+            StringBuilder str = new StringBuilder();
+            char[] buffer = new char[1024];
+            while ((reader.read(buffer) != -1)) {
+                str.append(buffer);
+            }
+            response.success = true;
+            response.data = str.toString();
+        } catch (FileNotFoundException err) {
+            response.success = false;
+            response.data = "File does not exist";
+        } catch (IOException err) {
+            response.success = false;
+            response.data = "File reading failed";
+        }
+        return response;
     }
 }
